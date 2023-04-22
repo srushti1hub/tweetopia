6150eed8f78c06c95f25498ed860a4eb355680b7
srushti1hub <srushtirtv18@gmail.com>
2023-04-22 12:16:11 +0530

    custom hooks

---
+++ hooks/useUserInfo.js
@@ -0,0 +1,27 @@
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+export default function useUserInfo(){
+    const{ data: session, status: sessionStatus } = useSession();
+    const[userInfo,setUserInfo] = useState();
+    const[status,setStatus] = useState('loading');
+    
+    function getUserInfo(){
+        if(sessionStatus === 'loading' || !session){
+            return;
+        }
+        fetch('/api/users?id='+session.user.id)
+        .then(response => {
+            response.json().then(json => {
+                setUserInfo(json.user)
+                setStatus('authenticated');
+            }) 
+        })
+    }
+
+    useEffect(()=> {
+        getUserInfo();
+    }, [sessionStatus]);
+    
+    return{userInfo,status}
+}
